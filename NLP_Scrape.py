edc3baf231d2fd761630e3e96015687d2f4fb5a5
Josh Farr <jfarr001@odu.edu>
2022-03-29 11:22:19 -0400

    Adding Both Data Sheets

--- NLP_Scrape.py
+++ NLP_Scrape.py
@@ -4,11 +4,22 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 if __name__=="__main__":
-    data=pd.read_csv('NLP Project Raw Data_Incomplete - Sheet1.csv')
-    print(data['C/I'])
+    data=pd.read_csv('NLP_Project_Raw_Data_Incomplete.csv')
+    data['C/I'] = 'I'
     #date = find_date('https://globalnews.ca/news/8453515/omicron-covid-variant-explained/')
     #print(date)
     #r.encode('utf-8')
     #print(r)
+    dates=[]
     #soup = BeautifulSoup(r.content, features='html.parser')
+    for l in data['Full Text Link']:
+        date = find_date(l)
+        dates.append(date)
+        r=requests.get(l)
+        soup = BeautifulSoup(r.content, features='html.parser')
+
+    data['Date'] = dates
+    #print(data['Date'])
+    
+    
     #print(r.text)
