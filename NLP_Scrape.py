ffeec3553e9afd33309fc1f4e02f2f7012217d1f
Josh Farr <jfarr001@odu.edu>
2022-03-29 13:02:39 -0400

    Able to Update Dates in CSV Files

--- NLP_Scrape.py
+++ NLP_Scrape.py
@@ -2,24 +2,31 @@ import requests
 from htmldate import find_date
 from bs4 import BeautifulSoup
 import pandas as pd
+import html2text
 
 if __name__=="__main__":
     data=pd.read_csv('NLP_Project_Raw_Data_Incomplete.csv')
-    data['C/I'] = 'I'
     #date = find_date('https://globalnews.ca/news/8453515/omicron-covid-variant-explained/')
     #print(date)
     #r.encode('utf-8')
     #print(r)
-    dates=[]
+    #dates=[]
+    ftexts=[]
     #soup = BeautifulSoup(r.content, features='html.parser')
     for l in data['Full Text Link']:
-        date = find_date(l)
-        dates.append(date)
+        ftext=""
+        #date = find_date(l)
+        #dates.append(date)
         r=requests.get(l)
+        h=html2text.HTML2Text()
+        h.ignore_links=True
         soup = BeautifulSoup(r.content, features='html.parser')
+        ftext= h.handle(r.text)
+        #ftexts.append(ftext)
 
-    data['Date'] = dates
-    #print(data['Date'])
+    #data['Date'] = dates
+    data['Full Text'] = ftexts
+    #data.to_csv('NLP_Project_Raw_Data_Incomplete.csv')
     
     
     #print(r.text)
