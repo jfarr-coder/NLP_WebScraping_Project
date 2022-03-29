88e5d692203b51ea269546a4b4e06635f1e93c67
Josh Farr <jfarr001@odu.edu>
2022-03-29 13:31:49 -0400

    Able to Get Full Text From Links

--- NLP_Scrape.py
+++ NLP_Scrape.py
@@ -5,13 +5,14 @@ import pandas as pd
 import html2text
 
 if __name__=="__main__":
-    data=pd.read_csv('NLP_Project_Raw_Data_Incomplete.csv')
+    data=pd.read_csv('NLP_Project_Raw_Data_Complete.csv')
     #date = find_date('https://globalnews.ca/news/8453515/omicron-covid-variant-explained/')
     #print(date)
     #r.encode('utf-8')
     #print(r)
     #dates=[]
     ftexts=[]
+    data = data.dropna(subset=['Full Text Link'])
     #soup = BeautifulSoup(r.content, features='html.parser')
     for l in data['Full Text Link']:
         ftext=""
@@ -21,12 +22,12 @@ if __name__=="__main__":
         h=html2text.HTML2Text()
         h.ignore_links=True
         soup = BeautifulSoup(r.content, features='html.parser')
-        ftext= h.handle(r.text)
-        #ftexts.append(ftext)
+        ftext= soup.get_text()
+        ftexts.append(ftext.strip().encode("utf-8").decode("utf-8"))
 
     #data['Date'] = dates
     data['Full Text'] = ftexts
-    #data.to_csv('NLP_Project_Raw_Data_Incomplete.csv')
+    data.to_csv('NLP_Project_Raw_Data_Complete.csv')
     
     
     #print(r.text)
