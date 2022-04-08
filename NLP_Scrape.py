bab3f9086d653377e43123d8079663f8c86a4db2
jfarr-coder <jfarr001@odu.edu>
2022-04-07 22:59:09 -0400

    Cleaning Up Code and Preparing it for ML

--- NLP_Scrape.py
+++ NLP_Scrape.py
@@ -3,32 +3,23 @@ from htmldate import find_date
 from bs4 import BeautifulSoup
 import pandas as pd
 #import html2text
-#import nltk
+import nltk
 
-if __name__=="__main__":
-    data=pd.read_csv('NLP_Project_Raw_Data_Incomplete.csv')
-    #date = find_date('https://globalnews.ca/news/8453515/omicron-covid-variant-explained/')
-    #print(date)
-    #r.encode('utf-8')
-    #print(r)
+def getDates(filename):
+    data=pd.read_csv(filename)
     dates=[]
-    #ftexts=[]
     data = data.dropna(subset=['Full Text Link'])
-    #soup = BeautifulSoup(r.content, features='html.parser')
     for l in data['Full Text Link']:
-        #ftext=""
         date = find_date(l)
         dates.append(date)
-        #r=requests.get(l)
-        #h=html2text.HTML2Text()
-        #h.ignore_links=True
-        #soup = BeautifulSoup(r.content, features='html.parser')
-        #ftext= soup.get_text()
-        #ftexts.append(ftext.strip().encode("utf-8").decode("utf-8"))
+    data['Date'] = dates
+    data.to_csv(filename)
 
     data['Date'] = dates
-    #data['Full Text'] = ftexts
-    data.to_csv('NLP_Project_Raw_Data_Incomplete.csv')
+
+if __name__=="__main__":
+    #getDates('NLP_Project_Raw_Data_Complete.csv')
+    #getDates('NLP_Project_Raw_Data_Incomplete.csv')
     
-    
-    #print(r.text)
+    complete = pd.read_csv('NLP_Project_Raw_Data_Complete.csv')
+    Incomplete = pd.read_csv('NLP_Project_Raw_Data_Incomplete.csv')
