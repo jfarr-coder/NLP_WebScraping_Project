ec04770ad3e7ea14be4bdea50efffdf0e13e087a
jfarr-coder <jfarr001@odu.edu>
2022-04-09 16:43:02 -0400

    Able to Get Labels, Features, and Training/Test DataFrames

--- NLP_Classfier.py
+++ NLP_Classfier.py
@@ -1,10 +1,27 @@
-import requests
-from htmldate import find_date
-from bs4 import BeautifulSoup
+import numpy as np
 import pandas as pd
 #import html2text
 import nltk
 import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+#https://towardsdatascience.com/machine-learning-nlp-text-classification-using-scikit-learn-python-and-nltk-c52b92a7c73a
+def getLabelsFeatures(dataframe):
+    label_names = ['Complete','Incomplete']
+    label = dataframe['C/I']
+    feature_names = ['Headline','C/I']
+    features = dataframe[feature_names]
+    return label_names, label, feature_names, features
+
+def getALLData(filename, filename2):
+    data=pd.read_csv(filename).dropna()
+    data2=pd.read_csv(filename2).dropna()
+    all_data = pd.concat([data, data2], axis=0).drop('Answer Portions',axis=1)
+    return all_data
 
 def getData(filename):
     data=pd.read_csv(filename)
@@ -26,19 +43,33 @@ def getDates(filename):
 if __name__=="__main__":
     complete_training,complete_testing = getData('NLP_Project_Raw_Data_Complete.csv')
     incomplete_training,incomplete_testing = getData('NLP_Project_Raw_Data_Incomplete.csv')
-    label_names = ['Complete','Incomplete']
-    label = complete['C/I']
-    feature_names = ['Headline','C/I']
-    features = complete[feature_names]
 
-    print(complete_training)
-    print(complete_testing)
-
-    print(incomplete_training)
-    print(incomplete_testing)
 
     complete = pd.read_csv('NLP_Project_Raw_Data_Complete.csv').dropna()
     Incomplete = pd.read_csv('NLP_Project_Raw_Data_Incomplete.csv').dropna()
-    #print(complete)
-    #print(Incomplete)
+
+    all_data = pd.concat([complete, Incomplete], axis=0).drop('Answer Portions',axis=1)
+    #print(all_data)
+    label_names = ['Complete','Incomplete']
+    labels = all_data['C/I']
+    feature_names = ['Headline','C/I']
+    features = all_data[feature_names]
+
+    train, test, train_labels, test_labels = train_test_split(features,
+                                                          labels,
+                                                          test_size=0.33,
+                                                          random_state=42)
     
+
+    print(train)
+    print(train_labels)
+    print(test)
+    print(test_labels)
+
+    #TfidfVectorizer= TfidfVectorizer()
+    #count_vect = CountVectorizer()
+    #X_train_counts = count_vect.fit_transform(train)
+    #print(X_train_counts)
+    #tfidf_transformer = TfidfTransformer()
+    #X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+    #print(X_train_tfidf)
--- WebScrape.py
+++ WebScrape.py
@@ -3,8 +3,6 @@ from htmldate import find_date
 from bs4 import BeautifulSoup
 import pandas as pd
 #import html2text
-import nltk
-import sklearn
 
 def getDates(filename):
     data=pd.read_csv(filename)
@@ -15,6 +13,7 @@ def getDates(filename):
         dates.append(date)
     data['Date'] = dates
     data.to_csv(filename)
+    return data
 
 if __name__=="__main__":
     getDates('NLP_Project_Raw_Data_Complete.csv')
