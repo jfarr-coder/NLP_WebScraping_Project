5c94ba259db31d1681474744bd8945f197d79bf3
jfarr-coder <jfarr001@odu.edu>
2022-04-14 23:22:50 -0400

    Implemented POS Tagging and TF

--- NLP_Classfier.py
+++ NLP_Classfier.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import nltk
 import sklearn
+import requests
 from sklearn import svm
 from sklearn import tree
 from sklearn.model_selection import train_test_split
@@ -21,6 +22,41 @@ from nltk.tokenize import word_tokenize, sent_tokenize
 #nltk.download('averaged_perceptron_tagger')
 #https://towardsdatascience.com/machine-learning-nlp-text-classification-using-scikit-learn-python-and-nltk-c52b92a7c73a
 #https://www.nltk.org/api/nltk.classify.scikitlearn.html
+
+def tag_sentence(sentence):
+    tags = [] 
+    terms=[]
+    tokenize = sent_tokenize(sentence)
+    for i in tokenize:
+        # Word tokenizers is used to find the words
+        # and punctuation in a string
+        wordsList = nltk.word_tokenize(i)
+
+        tagged = nltk.pos_tag(wordsList)
+        for t in tagged:
+            tags.append(t[1])
+            terms.append(t[0])
+    return tags, terms
+
+def getDictionary():
+    #requests.get("https://github.com/first20hours/google-10000-english/blob/master/20k.txt")
+    #requests.get("https://raw.githubusercontent.com/dwyl/english-words/master/words.txt")
+    #file1.text
+    # open("unigrams.txt","r")
+    # open("terms.txt","r")
+    # file1.readlines()
+    file1=open("terms.txt","r")
+    unigrams=[]
+    for l in file1.readlines():
+        unigrams.append(l.strip())
+    return unigrams
+
+def getPOS_Tags():
+    tags=[]
+    file1=open("POS_Tags.txt","r")
+    for l in file1.readlines():
+        tags.append(l.strip())
+    return tags
 
 def getLabelsFeatures(dataframe):
     label_names = ['Complete','Incomplete']
@@ -52,15 +88,57 @@ def getDates(filename):
     data['Date'] = dates
     data.to_csv(filename)
 
+#https://www.geeksforgeeks.org/part-speech-tagging-stop-words-using-nltk-python/
+#https://www.geeksforgeeks.org/python-ways-to-remove-duplicates-from-list/
+def getTermsDataFrame(data,pos_tags,unigrams):
+    stop_words = set(stopwords.words('english'))
+    #features = data['Headline']
+    columns=[]
+    all_terms=[]
+    columns.append('Headline')
+    original_size=len(data['Headline'])
+    for p in pos_tags:
+        probs=[]
+        columns.append(p)
+        for f in range(original_size):
+            s = data['Headline'][f]
+            tags,terms=tag_sentence(s)
+            terms_num=len(terms) 
+            tf = tags.count(p)
+            ntf = tf/terms_num
+            probs.append(ntf)
+            for t in terms:
+                all_terms.append(t)
+        data[p]=probs
+
+    for u in unigrams:
+        probs=[]
+        columns.append(u)
+        for f in range(original_size):
+            s = data['Headline'][f]
+            tags,terms=tag_sentence(s)
+            terms_num=len(terms) 
+            tf = terms.count(u)
+            ntf = tf/terms_num
+            probs.append(ntf)
+        data[u]=probs
+    #all_terms = list(set(all_terms))
+    #for a in all_terms:
+    #    print(a)
+    columns.append('C/I')
+    #terms = list(set(terms))
+    features=data[columns]
+    #tags_df = pd.DataFrame(features, index =features,columns =terms)
+    return features
+
 if __name__=="__main__":
     complete_training,complete_testing = getData('NLP_Project_Raw_Data_Complete.csv')
     incomplete_training,incomplete_testing = getData('NLP_Project_Raw_Data_Incomplete.csv')
 
-
     complete = pd.read_csv('NLP_Project_Raw_Data_Complete.csv').dropna()
     Incomplete = pd.read_csv('NLP_Project_Raw_Data_Incomplete.csv').dropna()
 
-    all_data = pd.concat([complete, Incomplete], axis=0).drop('Answer Portions',axis=1)
+    all_data = pd.concat([complete, Incomplete], axis=0).drop('Answer Portions',axis=1).reset_index()
     #print(all_data)
     label_names = ['Complete','Incomplete']
     labels = all_data['C/I']
@@ -68,46 +146,7 @@ if __name__=="__main__":
     #https://www.geeksforgeeks.org/part-speech-tagging-stop-words-using-nltk-python/
     #https://stackabuse.com/python-for-nlp-parts-of-speech-tagging-and-named-entity-recognition/
     #https://www.nltk.org/book/ch05.html
-    stop_words = set(stopwords.words('english'))
-    features = all_data['Headline']
-    print(all_data['Headline'])
-    #for f in features:
-    #    print(f)
-    #    tokenize = sent_tokenize(f)
-    #    print(tokenize)
 
-    train, test, train_labels, test_labels = train_test_split(features,
-                                                          labels,
-                                                          test_size=0.33,
-                                                          random_state=42)
-    
-
-    #print(train)
-    #print(train_labels)
-    #print(test)
-    #print(test_labels)
-
-    TfidfVectorizer= TfidfVectorizer()
-    count_vect = CountVectorizer()
-    X_train_counts = count_vect.fit_transform(train['Headline'])
-    #print(X_train_counts.shape)
-    tfidf_transformer = TfidfTransformer()
-    X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts) # DO NOT USE (SVM, Decistion Trees)
-    #SVM
-    #https://scikit-learn.org/stable/modules/svm.html
-    #https://www.datacamp.com/community/tutorials/svm-classification-scikit-learn-python
-    #https://towardsdatascience.com/support-vector-machines-explained-with-python-examples-cb65e8172c85
-    #Decision Trees
-    #https://scikit-learn.org/stable/modules/tree.html
-    #https://www.w3schools.com/python/python_ml_decision_tree.asp
-    #https://www.datacamp.com/community/tutorials/decision-tree-classification-python
-    #https://www.geeksforgeeks.org/decision-tree-implementation-python/
-    #SVM Decision Trees
-    #https://towardsdatascience.com/a-complete-view-of-decision-trees-and-svm-in-machine-learning-f9f3d19a337b
-    #https://towardsdatascience.com/ensemble-learning-with-support-vector-machines-and-decision-trees-88f8a1b5f84b
-    #https://www.numpyninja.com/post/a-simple-introduction-to-decision-tree-and-support-vector-machines-svm
-    #https://www.codementor.io/blog/text-classification-6mmol0q8oj
-    #https://scikit-learn.org/stable/modules/generated/sklearn.tree.DecisionTreeClassifier.html
-    #print(X_train_tfidf)
-    #clf = MultinomialNB().fit(X_train_tfidf, X_train_counts)
-    #print(clf)
+    #print(getDictionary())
+    terms_df = getTermsDataFrame(all_data,getPOS_Tags(),getDictionary())
+    print(terms_df)
